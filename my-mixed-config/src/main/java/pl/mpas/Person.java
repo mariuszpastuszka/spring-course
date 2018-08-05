d9ff2c64776384434c6c780c096ea043428d8cfb
maniek <mpastuszka@gmail.com>
2018-08-05 15:47:31 +0200

    [fix] - fixing @Value default value

--- my-mixed-config/src/main/java/pl/mpas/Person.java
+++ my-mixed-config/src/main/java/pl/mpas/Person.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Person {
-    @Value("name")
+    @Value("${name:nie-ma}")
     private String name;
 
     @Value("${surname}")
