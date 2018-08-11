fec38f0c4b73ba4fe5c72fc7f44d6b7dc6722aa1
maniek <mpastuszka@gmail.com>
2018-08-11 11:15:42 +0200

    [add] - test for car

--- my-car-xml/src/test/java/pl/mpas/car/MyTest.java
+++ my-car-xml/src/test/java/pl/mpas/car/MyTest.java
@@ -1,15 +1,22 @@
 package pl.mpas.car;
 
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = MyConfig.class)
+@ContextConfiguration("/my-beans.xml")
 public class MyTest {
 
     @Autowired
     Car car;
+
+    @Test
+    public void checkReference() {
+        Assert.assertNotNull(car);
+    }
 }
