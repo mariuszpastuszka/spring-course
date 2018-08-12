4cfaa9609e4e291a15b66faaa6760f6ce49ea4fc
maniek <mpastuszka@gmail.com>
2018-08-12 10:40:31 +0200

    [add] - student model class

---
+++ simple-crud/src/main/java/pl/mpas/crud/domain/Student.java
@@ -0,0 +1,10 @@
+package pl.mpas.crud.domain;
+
+public class Student {
+
+    private Long id;
+    private String name;
+    private String surname;
+    private String grade;
+    private String faculty;
+}
