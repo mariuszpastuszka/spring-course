95a8f112053aa5ec66089f88129eacc5315e6217
maniek <mpastuszka@gmail.com>
2018-08-12 10:11:34 +0200

    [fix] - dependency fix

--- simple-crud/src/main/java/pl/mpas/crud/controller/WelcomeController.java
+++ simple-crud/src/main/java/pl/mpas/crud/controller/WelcomeController.java
@@ -2,10 +2,14 @@ package pl.mpas.crud.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 
 public class WelcomeController {
 
     private static final Logger log = LoggerFactory.getLogger(WelcomeController.class);
 
-
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "welcome";
+    }
 }
