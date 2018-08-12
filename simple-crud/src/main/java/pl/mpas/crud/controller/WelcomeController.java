b385897fe71b07069041833f9475dca670229dc1
maniek <mpastuszka@gmail.com>
2018-08-12 10:05:39 +0200

    [fix] - fixing project structure

---
+++ simple-crud/src/main/java/pl/mpas/crud/controller/WelcomeController.java
@@ -0,0 +1,11 @@
+package pl.mpas.crud.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WelcomeController {
+
+    private static final Logger log = LoggerFactory.getLogger(WelcomeController.class);
+
+
+}
