5e72eb05edd939e7a5130fd001e839d9e7d47c18
maniek <mpastuszka@gmail.com>
2018-08-18 11:10:59 +0200

    [update] - rest-service update

--- rest-service-example/src/main/java/hello/Greeting.java
+++ rest-service-example/src/main/java/hello/Greeting.java
@@ -1,13 +1,33 @@
 package hello;
 
+class Person {
+    String name;
+    String surname;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+}
+
 public class Greeting {
 
     private final long id;
     private final String content;
+    private final Person person;
 
-    public Greeting(long id, String content) {
+    public Greeting(long id, String content, Person person) {
         this.id = id;
         this.content = content;
+        this.person = person;
     }
 
     public long getId() {
@@ -16,5 +36,9 @@ public class Greeting {
 
     public String getContent() {
         return content;
+    }
+
+    public Person getPerson() {
+        return person;
     }
 }
