3607f4e25daad672bbbc4980603296de0023456b
maniek <mpastuszka@gmail.com>
2018-09-02 10:49:32 +0200

    [add] - decision full example

--- decision-time/src/main/java/pl/mpas/decision/controller/DecisionController.java
+++ decision-time/src/main/java/pl/mpas/decision/controller/DecisionController.java
@@ -2,13 +2,24 @@ package pl.mpas.decision.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DecisionController {
 
-    @GetMapping({ "/welcome", "/" })
+    @GetMapping(value = { "/welcome", "/" })
     String welcome() {
         return "welcome";
     }
 
+    @PostMapping("/decision")
+    String makeDecision(@RequestParam(name = "decision") String decision) {
+        switch (decision) {
+            case "yes":
+                return "yes-html";
+            default:
+                return "no-html";
+        }
+    }
 }
