b86d103acd1d7def7723f1583b5d52a6b79bc4ea
maniek <mpastuszka@gmail.com>
2018-09-22 10:06:34 +0200

    [add] - file upload example

---
+++ file-upload-example/src/main/java/pl/mpas/storage/StorageException.java
@@ -0,0 +1,12 @@
+package pl.mpas.storage;
+
+public class StorageException extends RuntimeException {
+
+    public StorageException(String message) {
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
